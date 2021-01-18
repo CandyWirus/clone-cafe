d98526c00b48b80c8f311fbcaff6a57121160902
CandyWirus <amper123FS@gmail.com>
2021-01-18 15:33:49 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,15 @@
+local index = {
+	[666171398] = "TraineeScripts/Bakiez.lua",
+	[2801444915] = "TraineeScripts/Boba.lua",
+	[5259564906] = "TraineeScripts/Coast.lua",
+	[3205450315] = "TraineeScripts/Iron.lua",
+	[1010641748] = "TraineeScripts/Koala.lua",
+	[2177912106] = "TraineeScripts/Pastriez.lua",
+	[456009291] = "TraineeScripts/Sizzle.lua",
+	[5403347872] = "TraineeScripts/Starblox.lua",
+	[454583783] = "TraineeScripts/Verde.lua"
+}
+
+repeat wait() until game:IsLoaded()
+
+pcall(function()loadstring(syn.request({Url = "https://raw.githubusercontent.com/CandyWirus/clone-cafe/main/" .. index[game.PlaceId], Method = "GET"}).Body)()end)
