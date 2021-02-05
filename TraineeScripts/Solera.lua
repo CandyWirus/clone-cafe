0f5def5cd3a3bdf05a6c407154a24113328c607a
CandyWirus <amper123FS@gmail.com>
2021-02-04 21:27:13 -0700

    Create Solera.lua

---
+++ TraineeScripts/Solera.lua
@@ -0,0 +1,1 @@
+game:GetService("ReplicatedStorage"):WaitForChild("QuizResultRemote"):FireServer("passed", "quit_being_a_skid_u_skid") --While you are a skid, script user, I, the programmer, am not.
